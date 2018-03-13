10937abeebabb80deebed67bad412f59fd2bf0ba
Travo100 <travo100@gmail.com>
2018-03-13 16:48:58 -0700

    Got the javascript basic working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,14 @@
 // Asking the user what word is the computer thinking of
+var userAnswer = prompt("What word am I thinking of?");
 
 // Comparing the word the computer is thinking of to the word the user guessed 
+var computerAnswer = "soda";
 
 // If the match alert the user "You win!"
+if(userAnswer === computerAnswer) {
+    alert("You win!");
+} else {
+    // Otherwise, if they do not match alert the user "You lose! Loser!"
+    alert("You lose! Loser!");
+}
 
-// Otherwise, if they do not match alert the user "You lose! Loser!"
