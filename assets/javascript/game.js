7ae352fd35f9153a8e51d5ee16ee6b6ea9870954
Travo100 <travo100@gmail.com>
2018-03-13 16:43:55 -0700

    Added pseudocode

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,7 @@
-console.log("TEST");
+// Asking the user what word is the computer thinking of
+
+// Comparing the word the computer is thinking of to the word the user guessed 
+
+// If the match alert the user "You win!"
+
+// Otherwise, if they do not match alert the user "You lose! Loser!"
