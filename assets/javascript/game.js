a21cac4e6c4d5986bf2de91f87f1ef668b6a5745
Travo100 <travo100@gmail.com>
2018-03-13 17:20:40 -0700

    Removed prompt, using input field for user input

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,17 @@
-// Asking the user what word is the computer thinking of
-var userAnswer = prompt("What word am I thinking of?");
-
 // Comparing the word the computer is thinking of to the word the user guessed 
-var computerAnswer = "soda";
+var wordsArray = ["soda", "carrots", "guitar", "headphones", "chips", "bagels"];
 
-// If the match alert the user "You win!"
-if(userAnswer === computerAnswer) {
-    alert("You win!");
-} else {
-    // Otherwise, if they do not match alert the user "You lose! Loser!"
-    alert("You lose! Loser!");
+var computerAnswer = wordsArray[Math.floor(Math.random()*wordsArray.length)];
+
+console.log(computerAnswer);
+
+function handleUserGuess() {
+    var userAnswer = document.getElementById("guess").value;
+    // If the match alert the user "You win!"
+    if(userAnswer === computerAnswer) {
+        alert("You win!");
+    } else {
+        // Otherwise, if they do not match alert the user "You lose! Loser!"
+        alert("You lose! Loser!");
+    }
 }
-
